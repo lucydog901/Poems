4b41b5b8f22b07f4949b05ace561a79c8661d601
Thomasin Durgin <td@thomasin.com>
2019-12-06 01:15:17 +0000

    final

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -6,4 +6,16 @@ class PoemsController < ApplicationController
   def new
     @poem = Poem.new
   end
+
+  def create
+    Poem.create(poem_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def poem_params
+    params.require(:poem).permit(:title, :body, :author)
+  end
 end
+
